90bbf0007fc557fad2a506a4f6e0d3076866106f
Gabriel Retzlaff <gretzlaff13@gmail.com>
2022-08-17 22:20:45 -0300

    parte 2

--- assets/js/nbaAPI.js
+++ assets/js/nbaAPI.js
@@ -1,6 +1,7 @@
 const headers = new Headers();
-headeres.append("x-apisports-key", "6af85e3c2c76dfeec16dfae15aa5a121")
-headers.append("x-apisports-host", "v1.basketball.api-sports.io")
+headers.append("Accept", "*/*")
+headers.append("x-apisports-key", "6af85e3c2c76dfeec16dfae15aa5a121");
+
 
 var requestOptions = {
 	method: "GET",
@@ -8,13 +9,17 @@ var requestOptions = {
 	redirect: "follow" 
 };
 
-let endpoint = document.getElementById("inputPesquisa");
-console.log(`${endpoint}`)
 
-fetch(`https://v1.basketball.api-sports.io/${endpoint}`, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+const pesquisa = () => {
+  let endpoint = document.getElementById("inputPesquisa");
+  console.log(`${endpoint.value}`)
+  fetch(`https://v1.basketball.api-sports.io/${endpoint.value}`, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
+
+
 
 
 
