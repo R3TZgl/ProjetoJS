7d8ebd6c2ab1b8a135761ee66f9a08aaacff2a8b
Gabriel Retzlaff <gretzlaff13@gmail.com>
2022-08-22 21:42:30 -0300

    Pesquisas

--- assets/js/nbaAPI.js
+++ assets/js/nbaAPI.js
@@ -15,12 +15,26 @@ const pesquisa = () => {
   if(document.getElementById("escolha").value !== anterior){
     var select = document.getElementById("escolha").value;
     let endpoint = document.getElementById("inputPesquisa").value;
+
+    if(endpoint !== ""){
+      var url = `https://v1.basketball.api-sports.io/${select}?name=${endpoint}`;
+    }else{
+      var url = `https://v1.basketball.api-sports.io/${select}`;
+    }
+
     console.log(`${select}`)
-    fetch(`https://v1.basketball.api-sports.io/${select}?name=${endpoint}`, requestOptions)
+    fetch(url, requestOptions)
       .then(response => response.json())
       .then(result => {
         let info = result.response[0];
-        time(info)
+        console.log(result);
+        if(select === "teams"){
+          time(info);
+        }else if(select === "leagues"){
+          liga(info);
+        }else if(select === "countries"){
+          pais(info);
+        }
       })
       .catch(error => console.log('error', error));
       anterior = select;
@@ -31,12 +45,35 @@ const pesquisa = () => {
 
 const time = (info) => {
   let dados = document.getElementById("times");
-  console.log(info)
     dados.innerHTML = `
     <h1 id="nomeTime">${info.name}</h1>
     <img id="imgTime"
       src="${info.logo}"
-      width="200px" alt=""></img>`
+      width="200px" alt=""></img>
+      <h3 id="paisFooter">${info.country.name}</h3>
+      <img id="bandeiraFooter" src="${info.country.flag}" width="25px"> `
+}
+
+const liga = (info) => {
+  let dados = document.getElementById("times");
+    dados.innerHTML = `
+    <h1 id="nomeTime">${info.name}</h1>
+    <h3>${info.type}</h3>
+    <img id="imgTime"
+      src="${info.logo}"
+      width="200px" alt=""></img>
+      <h3 id="paisFooter">${info.country.name}</h3>
+      <img id="bandeiraFooter" src="${info.country.flag}" width="25px"> `
+}
+
+const pais = (info) => {
+  let dados = document.getElementById("times");
+    dados.innerHTML = `
+    <h1 id="nomeTime">${info.name}</h1>
+    <img id="imgTime"
+      src="${info.flag}"
+      width="200px" alt=""></img>
+      <h3 id="paisFooter">${info.code}</h3>`
 }
 
 
