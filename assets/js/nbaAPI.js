5998f194d78808e3293686af4ea18dcf78c1de10
Gustavo da Silva de Souza <gusssouza23@gmail.com>
2022-08-24 05:58:33 -0300

    Ultimas correções

--- assets/js/nbaAPI.js
+++ assets/js/nbaAPI.js
@@ -57,12 +57,12 @@ const pesquisaSelect = () => {
 const time = (info) => {
   let dados = document.getElementById("times");
   dados.innerHTML = `
-    <span id=displayNome><h1 id="nomeTime">${info.name}</h1><br>
-    <span id="coracaoFav" onclick="favoritos()" class="material-symbols-outlined">favorite</span></span>
+    <span id=displayNome><h1 id="nomeTime">${info.name}</h1><br></span>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <img id="imgTime"
       src="${info.logo}"
-      width="200px" alt=""></img>
+      width="200px" alt=""></img><br>
+      <span id="coracaoFav" onclick="favoritos()" class="material-symbols-outlined">favorite</span>
       <h3 id="paisFooter">${info.country.name}</h3>
       <img id="bandeiraFooter" src="${info.country.flag}" width="25px">`
 }
